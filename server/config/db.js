b326211cc40be52b9af3c4b494d0f0b3c73043d7
Atharva Patil <anpatil.1223@gmail.com>
2025-07-29 21:42:53 +0530

    add dotenv configuration and mongoose connection setup; update package.json with new dependencies and scripts

---
+++ server/config/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose"
+
+
+const connectDB = () => {
+    try {
+        mongo
+
+        console.log(first)
+    } catch (error) {
+        console.log('mongoDB connection error',error)
+        process.exit(1)
+    }
+}
