d9d6fba7abc57c0474bb6bdea51b2130ace2c94f
Atharva Patil <anpatil.1223@gmail.com>
2025-07-29 21:54:17 +0530

    establish database connection on server initialization

--- server/server.js
+++ server/server.js
@@ -1,8 +1,13 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv"
+import connectDB from "./config/db.js";
 
 dotenv.config()
+
+await connectDB()
+
+
 
 const port = process.env.PORT || 8080;
 
