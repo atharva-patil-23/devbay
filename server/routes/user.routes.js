07b897c74ca9f4367ac3ff877e838e179a5a2346
Atharva Patil <anpatil.1223@gmail.com>
2025-09-11 04:36:57 +0530

    add login route for user authentication

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -3,9 +3,9 @@ import jwt from "jsonwebtoken"
 import bcryptjs from "bcryptjs"
 import User from "../models/userModels.js"
 
-const router = express.Router()
+const userRouter = express.Router()
 
-router.post("/signup",async(req,res) => {
+userRouter.post("/signup",async(req,res) => {
 
     try {
         const {username , email , password} = req.body
@@ -14,25 +14,63 @@ router.post("/signup",async(req,res) => {
 
         const existingUser = await adminModel.findOne({email})
 
-    if(existingUser){
+        if(existingUser){
+            res.json({
+                message:"User already exists"
+            })
+        }
+
+        await User.create({
+            username:username,
+            email:email,
+            password:hashedPassword
+        })
+
+        res.status(200).json({
+            message:"Signup Successful!!"
+        })
+        
+        } catch (error) {
+            res.status(403).json({
+                message:"error while signing up"
+            })
+        }
+
+})
+
+userRouter.post("/login",async(req,res) => {
+    try {
+        const {email, password} = req.body
+
+        const user = await User.findOne({email})
+
+        if(!user){
+            res.status(409).json({
+                message:"invalid Credentials"
+            })
+        }
+
+        const validPassword = await bcryptjs.compare(password, user.password)
+
+        if(!validPassword){
+            res.status(402).json({
+                message:"Invalid Credentials"
+            })
+        }
+
+        const token = jwt.sign({
+            id:user._id
+        },process.env.JWT_USER_SECRET)
+
         res.json({
-            message:"User already exists"
+            token:token,
+            message:"Sigin IN successful!"
+        })
+
+
+    } catch (error) {
+        res.status(403).json({
+            message:"error while signing in!"
         })
     }
-
-    await adminModel.create({
-        username:username,
-        email:email,
-        password:hashedPassword
-    })
-
-    res.json({
-        message:"Signup Successful!!"
-    })
-        
-    } catch (error) {
-        
-    }
-    
-
 })
