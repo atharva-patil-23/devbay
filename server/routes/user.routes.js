594a3d63875245bd31b5a63fa4be148d364847c6
Atharva Patil <anpatil.1223@gmail.com>
2025-09-11 00:00:10 +0530

    add signup route to user routes for user registration

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -5,3 +5,34 @@ import User from "../models/userModels.js"
 
 const router = express.Router()
 
+router.post("/signup",async(req,res) => {
+
+    try {
+        const {username , email , password} = req.body
+
+        const hashedPassword = bcryptjs.hash(password,12)
+
+        const existingUser = await adminModel.findOne({email})
+
+    if(existingUser){
+        res.json({
+            message:"User already exists"
+        })
+    }
+
+    await adminModel.create({
+        username:username,
+        email:email,
+        password:hashedPassword
+    })
+
+    res.json({
+        message:"Signup Successful!!"
+    })
+        
+    } catch (error) {
+        
+    }
+    
+
+})
