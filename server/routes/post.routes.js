451176d1ff5f0c9c0fa46929c65f1998d4ff88f7
Atharva Patil <anpatil.1223@gmail.com>
2025-09-11 05:41:32 +0530

    add blog posting route with authentication and post creation logic

--- server/routes/post.routes.js
+++ server/routes/post.routes.js
@@ -1,11 +1,23 @@
 import express from "express"
 import authmiddleware from "../middleware/auth.middleware.js"
+import Post from "../models/postModel.js"
 
 const postRouter = express.Router()
 
 
-postRouter.post("/",authmiddleware,(req,res)=>{
+postRouter.post("/blog",authmiddleware,async(req,res)=>{
     const userId = req.userId
 
     const {title , content , createdBy} = req.body
+
+    const course = await Post.create({
+        title,
+        content,
+        createdBy
+    })
+
+    res.json({
+        message:"Blog posted successfully!"
+    })
+
 })
